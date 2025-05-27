56a837c03e48021df727ca7e36f78c93df580ad9
Tom Sydney Kerckhove <syd@cs-syd.eu>
2025-05-27 11:53:55 +0200

    remove now-obsolete explicit dependencies

--- flake.nix
+++ flake.nix
@@ -9,27 +9,18 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-25.05";
     pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
-    safe-coloured-text.url = "github:NorfairKing/safe-coloured-text";
-    safe-coloured-text.flake = false;
-    autodocodec.url = "github:NorfairKing/autodocodec";
-    autodocodec.flake = false;
     opt-env-conf.url = "github:NorfairKing/opt-env-conf";
     opt-env-conf.flake = false;
     looper.url = "github:NorfairKing/looper";
     looper.flake = false;
-    token-limiter-concurrent.url = "github:NorfairKing/token-limiter-concurrent";
-    token-limiter-concurrent.flake = false;
   };
 
   outputs =
     { self
     , nixpkgs
     , pre-commit-hooks
-    , safe-coloured-text
-    , autodocodec
     , opt-env-conf
     , looper
-    , token-limiter-concurrent
     }:
     let
       system = "x86_64-linux";
@@ -38,11 +29,8 @@
         config.allowUnfree = true;
         overlays = [
           (import ./nix/overlay.nix)
-          (import (safe-coloured-text + "/nix/overlay.nix"))
-          (import (autodocodec + "/nix/overlay.nix"))
           (import (opt-env-conf + "/nix/overlay.nix"))
           (import (looper + "/nix/overlay.nix"))
-          (import (token-limiter-concurrent + "/nix/overlay.nix"))
         ];
       };
     in
